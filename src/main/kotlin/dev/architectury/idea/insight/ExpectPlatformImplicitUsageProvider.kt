6e9041adcf6f4f189336461e0147a93a43354df0
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-10-06 21:45:58 -0400

    recursion 100

--- src/main/kotlin/dev/architectury/idea/insight/ExpectPlatformImplicitUsageProvider.kt
+++ src/main/kotlin/dev/architectury/idea/insight/ExpectPlatformImplicitUsageProvider.kt
@@ -12,24 +12,18 @@ class ExpectPlatformImplicitUsageProvider : ImplicitUsageProvider {
     override fun isImplicitUsage(element: PsiElement): Boolean {
         // if the element is a class, mark it as used if it has any ExpectPlatform methods
         if (element is PsiClass) {
-            return element.methods.any(::isImplicitUsage)
-        }
-
-        // if the method is implementing a common ExpectPlatform method mark it as used
-        if (element is PsiMethod) {
-            return element.commonMethods.isNotEmpty()
+            return element.methods.any { isImplicitUsage(it) }
         }
 
         // if the method is annotated with ExpectPlatform or implements an ExpectPlatform method,
         // mark all of its parameters as used.
         if (element is PsiParameter) {
             // the method is the parent's parent
-            val parent = element.parent.parent
-
-            return parent is PsiMethod && (parent.isCommonExpectPlatform || parent.commonMethods.isNotEmpty())
+            return isImplicitUsage(element.parent.parent)
         }
 
-        return false
+        // if the method is implementing a common ExpectPlatform method mark it as used
+        return element is PsiMethod && (element.isCommonExpectPlatform || element.commonMethods.isNotEmpty())
     }
 
     override fun isImplicitRead(element: PsiElement): Boolean = false
